99e3826bcc289f823ff3252c839d086fe57f36a0
Erik Slowikowski <e.slowikowski@bigpoint.net>
2017-01-26 14:28:29 +0100

    Added QuizButtonController

---
+++ Quiz/Assets/Scripts/Gameplay/QuizButtonController.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class QuizButtonController : MonoBehaviour {
+
+    [SerializeField]
+    private Text buttonLabel;
+
+    private bool correct = false;
+
+    public void Setup(string label)
+    {
+        buttonLabel.text = label;
+    }
+    public void SetCorrectOne()
+    {
+        correct = true;
+    }
+}
