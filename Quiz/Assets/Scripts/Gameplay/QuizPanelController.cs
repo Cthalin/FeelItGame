a34d8d88139b97fb2f6c5f57169b80e4cfd2a89d
Erik Slowikowski <e.slowikowski@bigpoint.net>
2017-01-26 14:29:07 +0100

    Added random generation & sorting of buttons

--- Quiz/Assets/Scripts/Gameplay/QuizPanelController.cs
+++ Quiz/Assets/Scripts/Gameplay/QuizPanelController.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
+using System;
 
 public class QuizPanelController : MonoBehaviour {
 
@@ -13,12 +14,55 @@ public class QuizPanelController : MonoBehaviour {
     private GameObject ButtonSpacer;
 
     private List<GameObject> quizButtonList = new List<GameObject>();
+    private List<string> trueFeelingsList = new List<string>();
+    private List<string> falseFeelingsList = new List<string>();
+    string[] rand = new string[4];
 
     public void Awake()
     {
-        for(int i = 0; i < 4; i++)
+        string[] lines = System.IO.File.ReadAllLines(@"Assets/Resources/TrueFeelings.txt");
+        for(int i = 0; i < lines.Length; i++)
         {
-            quizButtonList.Add(Instantiate(QuizButton, ButtonSpacer.transform));
+            trueFeelingsList.Add(lines[i]);
         }
+        lines = System.IO.File.ReadAllLines(@"Assets/Resources/FalseFeelings.txt");
+        for (int i = 0; i < lines.Length; i++)
+        {
+            falseFeelingsList.Add(lines[i]);
+        }
+
+        System.Random r = new System.Random();
+        rand[0] = trueFeelingsList[r.Next(trueFeelingsList.Count)];
+        rand[1] = trueFeelingsList[r.Next(falseFeelingsList.Count)];
+        rand[2] = trueFeelingsList[r.Next(falseFeelingsList.Count)];
+        rand[3] = trueFeelingsList[r.Next(falseFeelingsList.Count)];
+
+        for (int i = 0; i < 4; i++)
+        {
+            quizButtonList.Add(Instantiate(QuizButton));
+            quizButtonList[i].GetComponent<QuizButtonController>().Setup(rand[i]+" "+i); //TODO: Remove string addition! Just for debug
+        }
+
+        quizButtonList[0].GetComponent<QuizButtonController>().SetCorrectOne();
+        GameObject[] quizButtonArray = quizButtonList.ToArray();
+        shuffle(quizButtonArray);
+        for(int i = 0; i < quizButtonArray.Length; i++)
+        {
+            quizButtonArray[i].transform.SetParent(ButtonSpacer.transform);
+        }
+    }
+
+    GameObject[] shuffle(GameObject[] buttons)
+    {
+        // Knuth shuffle algorithm :: courtesy of Wikipedia :)
+        for (int t = 0; t < buttons.Length; t++)
+        {
+            GameObject tmp = buttons[t];
+            int r = UnityEngine.Random.Range(t, buttons.Length);
+            buttons[t] = buttons[r];
+            buttons[r] = tmp;
+        }
+
+        return buttons;
     }
 }
