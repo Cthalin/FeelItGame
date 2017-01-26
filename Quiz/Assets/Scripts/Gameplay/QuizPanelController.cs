add858dce6209a9b27eada7263bb67aad168c1f3
Erik Slowikowski <e.slowikowski@bigpoint.net>
2017-01-26 10:58:14 +0100

    Spawning 4 quiz buttons

---
+++ Quiz/Assets/Scripts/Gameplay/QuizPanelController.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class QuizPanelController : MonoBehaviour {
+
+	[SerializeField]
+    private GameObject QuizButton;
+    [SerializeField]
+    private Text ButtonLabel;
+    [SerializeField]
+    private GameObject ButtonSpacer;
+
+    private List<GameObject> quizButtonList = new List<GameObject>();
+
+    public void Awake()
+    {
+        for(int i = 0; i < 4; i++)
+        {
+            quizButtonList.Add(Instantiate(QuizButton, ButtonSpacer.transform));
+        }
+    }
+}
